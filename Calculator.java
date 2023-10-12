ad2d6e521c5c32380847e7b9b391a01eaaa14dff
ValerieMalicka <vqm5328@psu.edu>
2023-10-12 12:17:10 -0400

    calculator homework

--- Calculator.java
+++ Calculator.java
@@ -1,43 +1,69 @@
 import java.lang.Math;
 public class Calculator {
     private double memory;
+    private double memoryValue;
+
     public Calculator() {
         memory = 0;
+        memoryValue = 0;
     }
-    public double memoryPlus(double value, double num2) {
-        memory += value;
-        return value;
+
+
+    public void memoryPlus(double value) {
+        memoryValue += value;
+        System.out.println("Memory Plus: " + value);
     }
-    public void memoryMinus(double value, double num2) {
-        memory -= value;
+
+    public void memoryMinus(double value) {
+        memoryValue -= value;
+        System.out.println("Memory Minus: " + value);
     }
-    public void memoryStore(double value, double num2) {
-        memory = value;
+
+    public void memoryStore() {
+        memory = memoryValue;
+        System.out.println("Memory Stored: " + memory);
     }
-    public double memoryRecall(double num1, double num2) {
+
+    public double memoryRecall() {
         return memory;
     }
-    public double percent(double value, double percentage) {
-        return value * (percentage / 100);
-    }
-    public double divide(double dividend, double divisor) {
-        return dividend / divisor;
-    }
-    public double multiply(double num1, double num2) {
-        return num1 * num2;
-    }
+
     public double add(double num1, double num2) {
         return num1 + num2;
     }
+
     public double subtract(double num1, double num2) {
         return num1 - num2;
     }
-    public double squareRoot(double value) {
-        return Math.sqrt(value);
+
+    public double multiplication(double num1, double num2) {
+        return num1 * num2;
     }
+
+    public double divide(double dividend, double divisor) {
+        if (divisor != 0) {
+            return dividend / divisor;
+        } else {
+            System.out.println("Cannot divide by zero.");
+            return Double.NaN; // NOT-A-NUMBER [NaN]
+        }
+    }
+
+    public double squareRoot(double value) {
+        if (value >= 0) {
+            return Math.sqrt(value);
+        } else {
+            System.out.println("Cannot calculate square root of a negative number.");
+            return Double.NaN; // NOT-A-NUMBER [NaN]
+        }
+    }
+
     public double power(double base, double exponent) {
         return Math.pow(base, exponent);
     }
 
+    public double addPercentage(double number, double percentage) {
+        double decimalPercentage = percentage / 100;
+        return number + (number * decimalPercentage);
+    }
 }
-
--- Main.java
+++ Main.java
@@ -5,44 +5,96 @@ public class Main {
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter two numbers: ");
-        double num1 = scanner.nextDouble();
-        double num2 = scanner.nextDouble();
+        boolean shouldQuit = false;
 
-        double result = calculator.add(num1, num2);
-        System.out.println("Addition: " + result);
+        while (!shouldQuit) {
+            System.out.println("Menu:");
+            System.out.println("1. Perform calculations");
+            System.out.println("2. Memory operations");
+            System.out.println("3. Quit");
+            System.out.print("Enter your choice: ");
 
-       result = calculator.memoryPlus(num1,num2);
-        System.out.println("Memory Plus: " + result);
+            int choice = scanner.nextInt();
 
-        calculator.memoryMinus(num1,num2);
-        System.out.println("Memory Minus: " + result);
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter two numbers: ");
+                    double num1 = scanner.nextDouble();
+                    double num2 = scanner.nextDouble();
 
-        calculator.memoryStore(num1,num2);
-        System.out.println("Memory Store: " + result);
+                    double result = calculator.add(num1, num2);
+                    System.out.println("Addition: " + result);
 
-        calculator.memoryRecall(num1,num2);
-        System.out.println("Memory Recall: " + result);
+                    result = calculator.subtract(num1, num2);
+                    System.out.println("Subtraction: " + result);
 
-        result = calculator.percent(num1,num2);
-        System.out.println("Percent: " + result);
+                    result = calculator.multiplication(num1, num2);
+                    System.out.println("Multiplication: " + result);
 
-        result = calculator.subtract(num1, num2);
-        System.out.println("Subtraction: " + result);
+                    result = calculator.divide(num1, num2);
+                    System.out.println("Division: " + result);
 
-        result = calculator.multiply(num1, num2);
-        System.out.println("Multiplication: " + result);
+                    result = calculator.squareRoot(num1);
+                    System.out.println("Square Root of " + num1 + ": " + result);
 
-        result = calculator.divide(num1, num2);
-        System.out.println("Division: " + result);
+                    result = calculator.power(num1, num2);
+                    System.out.println(num1 + " raised to the power of " + num2 + ": " + result);
 
-        result = calculator.squareRoot(num1);
-        System.out.println("Square Root of " + num1 + ": " + result);
+                    System.out.print("Enter a number to add a percentage to: ");
+                    double numberToAddPercentageTo = scanner.nextDouble();
+                    System.out.print("Enter the percentage to add: ");
+                    double percentageToAdd = scanner.nextDouble();
 
-        result = calculator.power(num1, num2);
-        System.out.println(num1 + " raised to the power of " + num2 + ": " + result);
+                    result = calculator.addPercentage(numberToAddPercentageTo, percentageToAdd);
+                    System.out.println("Result after adding " + percentageToAdd + "%: " + result);
+                    break;
+
+                case 2:
+                    System.out.println("Memory: " + calculator.memoryRecall());
+                    System.out.println("1. Memory Plus");
+                    System.out.println("2. Memory Minus");
+                    System.out.println("3. Memory Store");
+                    System.out.println("4. Memory Recall");
+                    System.out.println("5. Back to Main Menu");
+                    System.out.print("Enter your choice: ");
+
+                    int memoryChoice = scanner.nextInt();
+
+                    switch (memoryChoice) {
+                        case 1:
+                            System.out.print("Enter value to add to memory: ");
+                            double valueToAdd = scanner.nextDouble();
+                            calculator.memoryPlus(valueToAdd);
+                            break;
+                        case 2:
+                            System.out.print("Enter value to subtract from memory: ");
+                            double valueToSubtract = scanner.nextDouble();
+                            calculator.memoryMinus(valueToSubtract);
+                            break;
+                        case 3:
+                            calculator.memoryStore();
+                            break;
+                        case 4:
+                            double memoryRecall = calculator.memoryRecall();
+                            System.out.println("Memory Recall: " + memoryRecall);
+                            break;
+                        case 5:
+// Return to the main menu
+                            break;
+                        default:
+                            System.out.println("Invalid choice.");
+                    }
+                    break;
+
+                case 3:
+                    shouldQuit = true;
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please enter a valid option.");
+            }
+        }
 
         scanner.close();
     }
-
 }
